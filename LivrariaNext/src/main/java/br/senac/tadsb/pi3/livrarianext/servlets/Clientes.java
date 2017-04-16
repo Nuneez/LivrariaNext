bb7698c9295a39648da900d059a9a858b143e8b4
Thiago Messias <messiasthi+thiagosmessias@gmail.com>
2017-04-16 16:29:30 -0300

    Arquivos

---
+++ LivrariaNext/src/main/java/br/senac/tadsb/pi3/livrarianext/servlets/Clientes.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senac.tadsb.pi3.livrarianext.servlets;
+
+import br.senac.tadsb.pi3.livrarianext.models.Cliente;
+import br.senac.tadsb.pi3.livrarianext.models.Perfil;
+import br.senac.tadsb.pi3.livrarianext.models.Usuario;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Thiago
+ */
+public class Clientes extends HttpServlet {
+  @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        Cliente cliente = new Cliente("Thiago", "Messias", "123.456.789-10", "01/01/1996", "masculino", "email@email.com", "+5511987654321");
+        // String nome, String sobreNome, String cpf, String nascimento, String sexo, String email, String telefone
+        request.setAttribute("cliente", cliente);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("Cliente.jsp");
+        
+        try
+        {
+            dispatcher.forward(request, response);
+        }
+        catch(IOException ex)
+        {
+            
+        }
+        
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+   
+    }
+}
