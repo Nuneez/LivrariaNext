3c27a6812ed0fe0aa08834a03081203aa7f2fab1
Thiago Messias <messiasthi+thiagosmessias@gmail.com>
2017-05-13 19:29:38 -0300

    Merge

--- LivrariaNext/src/main/webapp/scripts/pedido.js
+++ LivrariaNext/src/main/webapp/scripts/pedido.js
@@ -1,22 +1,13 @@
 window.onload = function() {
   window.liveSearch({
     cliente: function(element, callback) {
-      console.log("Cliente");
+      var url = '/LivrariaNext/ListarClientes?';
+      if (element.value)
       if (element.value.length > 3) {
-        // window.ajaxGet();
-        callback([{
-            name: 'aaaaaa',
-            id: 1
-          },
-          {
-            name: 'bbbbbb',
-            id: 2
-          },
-          {
-            name: 'cccccccc',
-            id: 3
-          }
-        ], element);
+        window.ajaxGet(, function() {
+          console.log()
+          // callback(, element);
+        });
       }
     },
     produto: function(element, callback) {
