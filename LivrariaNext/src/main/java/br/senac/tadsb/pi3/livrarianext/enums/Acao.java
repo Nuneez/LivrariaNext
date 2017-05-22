6c44bb68de63c492e355ecd0800231a52838744a
paulo.nunes <paulo.nunes@CAS511778W0734.senacsp.edu.br>
2017-05-22 20:29:27 -0300

    Correções

---
+++ LivrariaNext/src/main/java/br/senac/tadsb/pi3/livrarianext/enums/Acao.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senac.tadsb.pi3.livrarianext.enums;
+
+/**
+ *
+ * @author roger
+ */
+public enum Acao {
+    INSERT,
+    EDIT,
+    DELETE,
+    NONE
+}
